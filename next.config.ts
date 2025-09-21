e1885bd1df7055df2fdb95046e021aa0ee700138
Kiyan Alav <kiyanalavi1379@gmail.com>
2025-09-21 21:25:45 +0330

    - fix: update next config

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
-import nextPWA from "next-pwa";
-
-const withPWA = nextPWA({
-  dest: "public",
-  cacheOnFrontEndNav: true,
-  reloadOnOnline: true,
-  disable: process.env.NODE_ENV === "development",
-});
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default withPWA(nextConfig as any);
+nextConfig;
